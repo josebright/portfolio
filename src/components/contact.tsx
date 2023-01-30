0377d11d399de86179f96e6d51e06b07193aeff4
Uchenna Bright Ugwumadu <josebright29@gmail.com>
2023-01-30 22:44:07 +0300

    images

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,3 +1,41 @@
+import Image from 'next/image'
+
 export default function Contact() {
-    
+    return (
+        <>
+            <div>
+                <Image
+                    src=" "
+                    alt="email"
+                    width={500}
+                    height={500}
+                />
+            </div>
+            <div>
+                <Image
+                    src=" "
+                    alt="linkedin"
+                    width={500}
+                    height={500}
+                />
+            </div>
+            <div>
+                <Image
+                    src=" "
+                    alt="twitter"
+                    width={500}
+                    height={500}
+                />
+            </div>
+            <div>
+                <Image
+                    src=" "
+                    alt="github"
+                    width={500}
+                    height={500}
+                />
+            </div>
+        </>
+    )
+
 }
