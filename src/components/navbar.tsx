4dba2883481bd1b32a961fe90c58ea27a0dbd0a2
Uchenna Bright Ugwumadu <josebright29@gmail.com>
2023-01-30 23:05:07 +0300

    narbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,3 +1,12 @@
 export default function NavBar() {
-    
+    <>
+        <div>
+            
+        </div>
+        <div>
+            <div>About</div>
+            <div>Projects</div>
+            <div>Resume</div>
+        </div>
+    </>
 }
