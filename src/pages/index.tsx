ea0bd4c3946f9463075369fee3de41fd3396add3
Uchenna Bright Ugwumadu <josebright29@gmail.com>
2023-02-14 23:18:32 +0300

    tailwind configuration

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,8 @@
 import Head from 'next/head'
-// import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 import About from './about';
 
 
-// const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
 
