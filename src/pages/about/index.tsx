c6b296e418fae1d12160b85e5493448c59d0b739
Uchenna Bright Ugwumadu <josebright29@gmail.com>
2023-02-14 22:31:21 +0300

    adding linting rule along git validation

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -7,11 +7,11 @@ export default function About() {
         <div>
             <div>
                 <h2>Hello,</h2>
-                <h1>I'm { fName },</h1>
+                <h1>I amm { fName },</h1>
                 <h2></h2>
                 <p>
-                    I'm a mathematics graduate with over three years of software engineering expertise who is extremely self motivated and eager. 
-                    I love to solve problems, and I'm keen to improve my abilities and acquire knowledge in the
+                    I am a mathematics graduate with over three years of software engineering expertise who is extremely self motivated and eager. 
+                    I love to solve problems, and I am keen to improve my abilities and acquire knowledge in the
                     applications of mathematics and problem-solving techniques in technological engineering.
                 </p>
                 <p>
