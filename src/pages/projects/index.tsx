56730cf5134dfc5b850ff01ce42cff24e0e7db28
Uchenna Bright Ugwumadu <josebright29@gmail.com>
2023-03-06 23:56:16 +0300

    updated

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -3,12 +3,10 @@ export default function Projects() {
     <div>
       <h1 className="text-center font-bold text-4xl">PROJECTS</h1>
       <div className="grid grid-cols-2 gap-6 px-20 lg:grid-cols-3 lg:gap-4">
-        <div>01</div>
-        <div>02</div>
-        <div>03</div>
-        <div>04</div>
-        <div>05</div>
-        <div>06</div>
+        <div className="">01</div>
+        <div className="">02</div>
+        <div className="">03</div>
+        <div className="">04</div>
       </div>
     </div>
   )
